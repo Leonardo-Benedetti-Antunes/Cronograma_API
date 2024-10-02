d458f6f037acf0b4b57a9a80214f98a832db44c2
Leonardo Benedetti Antunes <leonardobenedettintunes@gmail.com>
2024-10-02 10:30:18 -0300

    Feat: commit de arquivos base do Spring Boot

---
+++ src/main/java/br/com/Cesurg/OrganizacaoDeCronograma/OrganizacaoDeCronogramaApplication.java
@@ -0,0 +1,13 @@
+package br.com.Cesurg.OrganizacaoDeCronograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizacaoDeCronogramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizacaoDeCronogramaApplication.class, args);
+	}
+
+}
